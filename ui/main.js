2d86dcfef4e7878c8a9d988cc0efe2b2a95cf448
upadhyayansh <anshumnupadhyay@gmail.com>
2018-02-20 19:50:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElemeneById('main-text');
 element.innerHTML='This is the new value which will be shown on the web browser now'
+var img = document.getElementById('img');
+img.onclick = function(){
+      img.style.marginLeft='100px';  
+};
